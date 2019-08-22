c9fe2336c149af00d8339a75cb6f02a90e69b94d
Kurt Hammes <kilowatt11@cableone.net>
2019-08-21 21:31:31 -0600

    Updates

---
+++ precache-manifest.39daf4dc6002eae373dcf5ce21156d10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1481a3f45d495c7a3c68f247c67bed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58573bd108e1bee00fca",
+    "url": "/monsters-rolodex/static/css/main.884bc86c.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "58573bd108e1bee00fca",
+    "url": "/monsters-rolodex/static/js/main.407c93e5.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
